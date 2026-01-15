12fcdf830a7c90bb6a86338e44abd7a23797db43
dakfildbanze3-design <dakfildbanze3@gmail.com>
2026-01-15 06:44:20 +0000

    oi

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fa6cb374'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3rmv4tg7h18"
+    "revision": "0.kclaqr4h1ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
